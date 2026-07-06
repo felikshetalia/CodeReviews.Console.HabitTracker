f710c1db8546dfe032783dab14b0cc830feef14d
felikshetalia <mccorsica@yahoo.com>
2026-07-06 11:29:49 +0200

    initial commit

---
+++ HabitTracker/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CodeReviews_Console_HabitTracker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
